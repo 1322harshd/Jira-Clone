390241f8ae2d096dd1541661f99cabf81fa2d45d
1322harshd <harsh.nz@outlook.com>
2026-05-28 09:32:47 +1200

    Initialized prisma and created first user model with successful migration

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+//get request
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT,() => {
+    console.log(`Server is running on port ${PORT}`);
+});
