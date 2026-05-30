061d654ea6e9df87ca7d1ea3cd2c1334c75a9bcf
1322harshd <harsh.nz@outlook.com>
2026-05-30 17:56:28 +1200

    added post route for adding user to database and used bcrypt to hash password

--- server.js
+++ server.js
@@ -1,12 +1,50 @@
 import express from 'express';
 import 'dotenv/config';
+import bcrypt from 'bcrypt';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from './generated/prisma/client.ts';
+
+const prisma = new PrismaClient({
+    adapter: new PrismaPg({
+        connectionString: process.env.DATABASE_URL
+    })
+});
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
-//get request
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+//for parsing json
+app.use(express.json());
+
+//signup route
+app.post('/users', async (req, res) => {
+    try{  
+        const {name,email,password}=req.body;
+        
+        if(!password){
+            return res.status(400).json({error:"Password is required"})
+        }
+
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password,saltRounds);
+
+        const newUser ={
+            name,
+            email,
+            password:hashedPassword
+        }
+
+        const result = await prisma.user.create({
+            data: newUser
+        });
+
+        res.status(201).json("User created successfully."); }
+
+        catch(error){
+            console.error(error);
+            res.status(500).json({error: "Internal server error"});
+        }
+
 });
 
 app.listen(PORT,() => {
