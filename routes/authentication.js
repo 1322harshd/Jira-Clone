39e76dcd3a2f12b39666bde7728636c042fc01c7
1322harshd <harsh.nz@outlook.com>
2026-05-31 18:08:55 +1200

    added login route with jwt token and created seprate folder and file for authentication routes

---
+++ routes/authentication.js
@@ -0,0 +1,88 @@
+import express from 'express';
+import bcrypt from 'bcrypt';
+import { PrismaClient } from '@prisma/client';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+
+const router = express.Router();
+
+const prisma = new PrismaClient();
+
+//signup route
+router.post('/users', async (req, res) => {
+    try{  
+        const {name,email,password}=req.body;
+        
+        if(!password){
+            return res.status(400).json({error:"Password is required"})
+        }
+
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password,saltRounds);
+
+        const newUser ={
+            name,
+            email,
+            password:hashedPassword
+        }
+
+        const result = await prisma.user.create({
+            data: newUser 
+            
+        });
+
+        console.log("User created Successfully!");
+        res.status(201).json("User created successfully."); }
+       
+        catch(error){
+            console.error(error);
+            res.status(500).json({error: "Internal server error"});
+        }
+
+});
+
+router.get('/login', async (req,res) => {
+    try{
+        const {email, password} = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({message: 'Email and password are required'});
+        }
+
+        const user = await prisma.user.findUnique({
+            where: {email:email}
+        })
+
+        if(!user){
+            return res.status(401).json({message: 'Invalid email or password'});
+        }
+
+        const isPasswordValid = await bcrypt.compare(password,user.password);
+        console.log(isPasswordValid);
+
+        if(!isPasswordValid){
+            return res.status(401).json({message: 'Invalid email or password'});
+        }
+        
+        const token = jwt.sign(
+            {userId:user.id,email:user.email},
+            process.env.JWT_SECRET,
+            {expiresIn:'1h'}
+        );
+
+        return res.status(200).json({
+            message:"Login successful. ",
+            token,
+            user:{id:user.id, email:user.email}
+        });
+
+
+    } catch(error){
+        console.error("Login error:",error);
+        return res.status(500).json({error: "An internal server occurred"});
+
+    }
+
+});
+
+export default router;
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 import express from 'express';
-import 'dotenv/config';
-import bcrypt from 'bcrypt';
-import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+import authentication from "./routes/authentication.js";
 
 const app = express();
 const PORT = process.env.PORT || 3002;
@@ -11,38 +8,7 @@ const PORT = process.env.PORT || 3002;
 //for parsing json
 app.use(express.json());
 
-//signup route
-app.post('/users', async (req, res) => {
-    try{  
-        const {name,email,password}=req.body;
-        
-        if(!password){
-            return res.status(400).json({error:"Password is required"})
-        }
-
-        const saltRounds = 10;
-        const hashedPassword = await bcrypt.hash(password,saltRounds);
-
-        const newUser ={
-            name,
-            email,
-            password:hashedPassword
-        }
-
-        const result = await prisma.user.create({
-            data: newUser 
-            
-        });
-
-        console.log("User created Successfully!");
-        res.status(201).json("User created successfully."); }
-       
-        catch(error){
-            console.error(error);
-            res.status(500).json({error: "Internal server error"});
-        }
-
-});
+app.use('/',authentication);
 
 app.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}`);
